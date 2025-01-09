5f85d9b3ebbb9df81195e91754c37e60d88a9ff6
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-01-10 00:36:08 +0600

    Home Page core UI decided

--- lib/src/screens/home/home_page.dart
+++ lib/src/screens/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +10,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController pageController = PageController();
+  int selectedBottomNavIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Al Quran"),
+      appBar: AppBar(
+        title: Text("Al Quran"),
+      ),
+      body: PageView(
+        onPageChanged: (value) {
+          setState(() {
+            selectedBottomNavIndex = value;
+          });
+        },
+        controller: pageController,
+        children: [
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.black,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      bottomNavigationBar: GNav(
+        selectedIndex: selectedBottomNavIndex,
+        rippleColor: Colors.grey, // tab button ripple color when pressed
+        hoverColor: Colors.grey, // tab button hover color
+        haptic: true, // haptic feedback
+        tabBorderRadius: 15,
+        tabActiveBorder:
+            Border.all(color: Colors.black, width: 1), // tab button border
+        tabBorder:
+            Border.all(color: Colors.grey, width: 1), // tab button border
+        tabShadow: [
+          BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
+        ], // tab button shadow
+        curve: Curves.easeOutExpo, // tab animation curves
+        duration: Duration(milliseconds: 900), // tab animation duration
+        gap: 8, // the tab button gap between icon and text
+        color: Colors.grey[800], // unselected icon color
+        activeColor: Colors.purple, // selected icon and text color
+        iconSize: 24, // tab button icon size
+        tabBackgroundColor:
+            Colors.purple.withOpacity(0.1), // selected tab background color
+        padding: EdgeInsets.symmetric(
+            horizontal: 20, vertical: 5), // navigation bar padding
+        tabs: [
+          GButton(
+            icon: LineIcons.home,
+            text: 'Home',
+          ),
+          GButton(
+            icon: LineIcons.heart,
+            text: 'Likes',
+          ),
+          GButton(
+            icon: LineIcons.search,
+            text: 'Search',
+          ),
+          GButton(
+            icon: LineIcons.user,
+            text: 'Profile',
+          )
+        ],
+        onTabChange: (value) {
+          pageController.jumpToPage(value);
+        },
       ),
     );
   }
