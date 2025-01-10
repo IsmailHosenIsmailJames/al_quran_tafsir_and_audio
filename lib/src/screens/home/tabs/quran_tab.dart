d8119c4ff39f5c6db0a9129df962de34996f9bf0
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-01-10 18:12:47 +0600

    UI enhanced

--- lib/src/screens/home/tabs/quran_tab.dart
+++ lib/src/screens/home/tabs/quran_tab.dart
@@ -101,53 +101,62 @@ class _QuranTabState extends State<QuranTab> {
                     return Container(
                       height: 55,
                       width: double.infinity,
-                      padding: EdgeInsets.all(5),
                       margin: EdgeInsets.only(left: 10, right: 10, top: 10),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(7),
                         color: Colors.grey.withValues(alpha: 0.2),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            child: Text(
-                              "${index + 1}",
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.all(5),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
                           ),
-                          Gap(10),
-                          Text(
-                            quranSurahInfoModel.nameSimple,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                quranSurahInfoModel.nameArabic,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              child: Text(
+                                "${index + 1}",
                                 style: TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Text(
-                                "${quranSurahInfoModel.versesCount} ayahs",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey.shade600,
+                            ),
+                            Gap(10),
+                            Text(
+                              quranSurahInfoModel.nameSimple,
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Spacer(),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  quranSurahInfoModel.nameArabic,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              )
-                            ],
-                          ),
-                        ],
+                                Text(
+                                  "${quranSurahInfoModel.versesCount} ayahs",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -161,42 +170,56 @@ class _QuranTabState extends State<QuranTab> {
                     return Container(
                       height: 55,
                       width: double.infinity,
-                      padding: EdgeInsets.all(5),
                       margin: EdgeInsets.only(left: 10, right: 10, top: 10),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(7),
                         color: Colors.grey.withValues(alpha: 0.2),
                       ),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            child: Text("${juzInfoModel.juzNumber}"),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.all(5),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
                           ),
-                          Gap(10),
-                          getJuzName(
-                            juzInfoModel,
-                            TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "${surahCountInJuz(juzInfoModel)} surahs",
-                              ),
-                              Text(
-                                "${getAyahCountJuz(juzInfoModel)} ayahs",
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              child: Text(
+                                "${juzInfoModel.juzNumber}",
                                 style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey.shade600,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                              )
-                            ],
-                          ),
-                        ],
+                              ),
+                            ),
+                            Gap(10),
+                            getJuzName(
+                              juzInfoModel,
+                              TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Spacer(),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${surahCountInJuz(juzInfoModel)} surahs",
+                                ),
+                                Text(
+                                  "${getAyahCountJuz(juzInfoModel)} ayahs",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
